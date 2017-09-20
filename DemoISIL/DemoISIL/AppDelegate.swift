694c9379e970a3e2146cab6fc6ae921ec18b3cf6
Alumno-DG <Alumno-DG@imac-de-b303-xx.local>
2017-09-19 20:10:01 -0500

    Resolución de conflictos

--- DemoISIL/DemoISIL/AppDelegate.swift
+++ DemoISIL/DemoISIL/AppDelegate.swift
@@ -9,6 +9,8 @@
 import UIKit
 import CoreData
 //commit y push
+//Peru va a ganar
+
 //HOLA MUCHACHOS ESTO ES UN CONFLICTO
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
